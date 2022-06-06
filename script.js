848dc76804ba856fc110ba1ec03a9f8363887349
Juan Vallejo <jvallejo.br@gmail.com>
2022-06-06 20:39:03 -0300

    Casi terminado

--- script.js
+++ script.js
@@ -23,24 +23,33 @@ form.addEventListener('change', () => {
 btncalcular.addEventListener('click', (e) => 
  
     calculateResult()
+   
   
 );
+
+function mostrar() {
+Num = 500;
+Num2 = 10;
+console.log(Num/Num2);
+
+}
+
 function calculateResult() {
   let EnTotalVALOR =  Number(cuenta.value);
   let CadaUnoValor = EnTotalVALOR / Number(Gente.value);
-
+  console.log('hola');
   EnTotalTITULO.textContent = 'En Total';
   EnTotalTITULO.classList.add('regular-title', 'text-left');
   EnTotalVALOR.textContent = (Math.round(EnTotalVALOR * 100) / 100).toFixed(2);
   EnTotalVALOR.classList.add('regular-title', 'text-right');
-
+  console.log('hola');
   CadaUnoTITULO.textContent = 'Total para cada uno';
   CadaUnoTITULO.classList.add('bold-title', 'text-left');
   CadaUnoValor.textContent = (
     Math.round(EnTotalVALOR * 100) / 100
   ).toFixed(2);
   CadaUnoValor.classList.add('bold-title', 'text-right');
-
+  console.log(CadaUnoValor);
   
 }
 
